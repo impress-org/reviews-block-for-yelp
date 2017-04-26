a8ac73fcc13b6d4d5014513db36edea687b25226
Devin Walker <sdtucson@gmail.com>
2017-04-26 16:13:29 -0700

    Improved constants, markdown

--- yelp-widget-pro.php
+++ yelp-widget-pro.php
@@ -1,34 +1,38 @@
 <?php
-/*
-Plugin Name: Yelp Widget Pro
-Plugin URI: http://wordpress.org/extend/plugins/yelp-widget-pro/
-Description: Easily display Yelp business ratings with a simple and intuitive WordPress widget.
-Version: 1.4.3
-Author: Devin Walker
-Author URI: http://wordimpress.com/
-License: GPLv2
-*/
+/**
+ * Plugin Name: Yelp Widget Pro
+ * Plugin URI: http://wordpress.org/extend/plugins/yelp-widget-pro/
+ * Description: Easily display Yelp business ratings with a simple and intuitive WordPress widget.
+ * Version: 1.4.3
+ * Author: Devin Walker
+ * Author URI: http://wordimpress.com/
+ * License: GPLv2
 
-/*
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; version 2 of the License.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 define( 'YELP_PLUGIN_NAME', 'yelp-widget-pro' );
-define( 'YELP_PLUGIN_NAME_PLUGIN', 'yelp-widget-pro/yelp-widget-pro.php' );
-define( 'YELP_WIDGET_PRO_PATH', WP_PLUGIN_DIR . '/' . YELP_PLUGIN_NAME );
-define( 'YELP_WIDGET_PRO_URL', WP_PLUGIN_URL . '/' . YELP_PLUGIN_NAME );
 
+if ( ! defined( 'YELP_PLUGIN_NAME_PLUGIN' ) ) {
+	define( 'YELP_PLUGIN_NAME_PLUGIN', plugin_basename( __FILE__ ) );
+}
+if ( ! defined( 'YELP_WIDGET_PRO_PATH' ) ) {
+	define( 'YELP_WIDGET_PRO_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+}
+if ( ! defined( 'YELP_WIDGET_PRO_URL' ) ) {
+	define( 'YELP_WIDGET_PRO_URL', plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) );
+}
 
 /**
  * Adds Yelp Widget Pro Options Page
@@ -53,7 +57,7 @@ function add_yelp_widget_css() {
 
 	$cssOption = get_option( 'yelp_widget_settings' );
 
-	if ( ! $cssOption || ! array_key_exists('yelp_widget_disable_css', $cssOption) ) {
+	if ( ! $cssOption || ! array_key_exists( 'yelp_widget_disable_css', $cssOption ) ) {
 
 		$url = plugins_url( YELP_PLUGIN_NAME . '/includes/style/yelp.css', dirname( __FILE__ ) );
 
@@ -71,6 +75,9 @@ if ( ! class_exists( 'Yelp_Widget' ) ) {
 	require 'widget.php';
 }
 
-if (is_admin()) {
+if ( is_admin() ) {
+	echo '<pre>';
+	var_dump(YELP_WIDGET_PRO_PATH . '/admin/admin.php');
+	echo '</pre>';
 	include YELP_WIDGET_PRO_PATH . '/admin/admin.php';
 }
