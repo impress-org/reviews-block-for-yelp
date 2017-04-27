23ea2be61d953baa238b3cee3a24a6f3229dbe9b
Devin Walker <sdtucson@gmail.com>
2017-04-26 17:23:12 -0700

    Register scripts before enqueing

--- includes/options.php
+++ includes/options.php
@@ -1,7 +1,6 @@
 <?php
 /**
- *  Admin options page. Creates a page to set your OAuth settings for the Yelp API v2.
- *
+ * Admin options page. Creates a page to set your OAuth settings for the Yelp API v2.
  */
 
 register_activation_hook( __FILE__, 'yelp_widget_activate' );
@@ -42,11 +41,14 @@ function yelp_widget_add_options_page() {
 
 }
 
-//Add Yelp Widget Pro option scripts to admin head - will only be loaded on plugin options page
+/**
+ * Add Yelp Widget Pro option scripts to admin head - will only be loaded on plugin options page
+ */
 function yelp_options_scripts() {
 
 	//register admin JS
-	wp_enqueue_script( 'yelp_widget_options_js', plugins_url( 'includes/js/options.js', dirname( __FILE__ ) ) );
+	wp_register_script( 'yelp_widget_options_js', plugins_url( 'includes/js/options.js', dirname( __FILE__ ) ) );
+	wp_enqueue_script( 'yelp_widget_options_js' );
 
 	//register our stylesheet
 	wp_register_style( 'yelp_widget_options_css', plugins_url( 'includes/style/options.css', dirname( __FILE__ ) ) );
@@ -59,10 +61,11 @@ function yelp_options_scripts() {
  */
 function yelp_widget_scripts( $hook ) {
 	if ( $hook == 'widgets.php' ) {
-		wp_enqueue_script( 'yelp_widget_admin_scripts', plugins_url( 'includes/js/admin-widget.js', dirname( __FILE__ ) ) );
-		wp_enqueue_style( 'yelp_widget_admin_css', plugins_url( 'includes/style/admin-widget.css', dirname( __FILE__ ) ) );
-	} else {
-		return;
+		wp_register_script( 'yelp_widget_admin_scripts', plugins_url( 'includes/js/admin-widget.js', dirname( __FILE__ ) ) );
+		wp_enqueue_script( 'yelp_widget_admin_scripts' );
+
+		wp_register_style( 'yelp_widget_admin_css', plugins_url( 'includes/style/admin-widget.css', dirname( __FILE__ ) ) );
+		wp_enqueue_style( 'yelp_widget_admin_css' );
 	}
 }
 
@@ -139,7 +142,7 @@ function yelp_widget_option( $setting, $options ) {
 	if ( get_option( $setting ) != '' ) {
 		$value = get_option( $setting );
 	} elseif ( is_array( $options ) ) {
-		$value = $options[$setting];
+		$value = $options[ $setting ];
 	}
 
 	return $value;
@@ -151,16 +154,17 @@ function yelp_widget_option( $setting, $options ) {
 function yelp_widget_options_form() {
 	?>
 
-	<div class="wrap" xmlns="http://www.w3.org/1999/html">
+    <div class="wrap" xmlns="http://www.w3.org/1999/html">
 
-		<!-- Plugin Title -->
-		<div id="ywp-title-wrap">
-			<div id="icon-yelp" class=""></div>
-			<h2><?php _e( 'Yelp Widget Pro Settings', 'ywp' ); ?> </h2>
-			<label class="label basic-label">Basic Version</label>
-			<a href="http://wordimpress.com/plugins/yelp-widget-pro/" title="Upgrade to Yelp Widget Premium" target="_blank" class="update-link new-window">Upgrade to Premium</a>
-		</div>
-		<form id="yelp-settings" method="post" action="options.php">
+        <!-- Plugin Title -->
+        <div id="ywp-title-wrap">
+            <div id="icon-yelp" class=""></div>
+            <h2><?php _e( 'Yelp Widget Pro Settings', 'ywp' ); ?> </h2>
+            <label class="label basic-label">Basic Version</label>
+            <a href="http://wordimpress.com/plugins/yelp-widget-pro/" title="Upgrade to Yelp Widget Premium"
+               target="_blank" class="update-link new-window">Upgrade to Premium</a>
+        </div>
+        <form id="yelp-settings" method="post" action="options.php">
 
 			<?php
 			// Tells Wordpress that the options we registered are being
@@ -170,134 +174,148 @@ function yelp_widget_options_form() {
 			// Retrieve stored options, if any
 			$options = get_option( 'yelp_widget_settings' ); ?>
 
-			<div class="metabox-holder">
+            <div class="metabox-holder">
 
-				<div class="postbox-container" style="width:75%">
-
-
-					<div id="main-sortables" class="meta-box-sortables ui-sortable">
-						<div class="postbox" id="yelp-widget-intro">
-							<div class="handlediv" title="Click to toggle"><br></div>
-							<h3 class="hndle"><span><?php _e( 'Yelp Widget Pro Introduction', 'ywp' ); ?></span></h3>
-
-							<div class="inside">
-								<p>
-								<?php
-								$widgets_url = admin_url( 'widgets.php' );
-								$link = sprintf( wp_kses( __( 'Thanks for choosing Yelp Widget Pro! To get started, head on over to your <a href="%s">Widgets page</a> and add Yelp Widget Pro to one of your active widget areas.', 'ywp' ), array(  'a' => array( 'href' => array() ) ) ), esc_url( $widgets_url ) );
-								echo $link;
-								?>
-								</p>
-
-								<p><strong><?php _e( 'Need Support?', 'ywp' ); ?></strong></p>
-
-								<p><?php _e( 'If you have any problems with this plugin or ideas for improvements, please use the <a href="https://wordpress.org/support/plugin/yelp-widget-pro">WordPress.org Support Forums</a> where you can search the existing topics or create one of your own.', 'ywp' ); ?></p>
-
-								<p>
-									<strong><?php _e( 'Like this plugin? Follow along with WordImpress:', 'ywp' ); ?></strong>
-								</p>
-
-								<div class="social-items-wrap">
-
-									<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FWordImpress%2F353658958080509&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=21&amp;appId=220596284639969" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>
-
-									<a href="https://twitter.com/wordimpress" class="twitter-follow-button" data-show-count="false">Follow @wordimpress</a>
-									<script>!function (d, s, id) {
-											var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
-											if (!d.getElementById(id)) {
-												js = d.createElement(s);
-												js.id = id;
-												js.src = p + '://platform.twitter.com/widgets.js';
-												fjs.parentNode.insertBefore(js, fjs);
-											}
-										}(document, 'script', 'twitter-wjs');</script>
-									<div class="google-plus">
-										<!-- Place this tag where you want the +1 button to render. -->
-										<div class="g-plusone" data-size="medium" data-annotation="inline" data-width="200" data-href="https://plus.google.com/117062083910623146392"></div>
+                <div class="postbox-container" style="width:75%">
 
 
-										<!-- Place this tag after the last +1 button tag. -->
-										<script type="text/javascript">
-											(function () {
-												var po = document.createElement('script');
-												po.type = 'text/javascript';
-												po.async = true;
-												po.src = 'https://apis.google.com/js/plusone.js';
-												var s = document.getElementsByTagName('script')[0];
-												s.parentNode.insertBefore(po, s);
-											})();
-										</script>
-									</div>
-									<!--/.google-plus -->
-								</div>
-								<!--/.social-items-wrap -->
+                    <div id="main-sortables" class="meta-box-sortables ui-sortable">
+                        <div class="postbox" id="yelp-widget-intro">
+                            <div class="handlediv" title="Click to toggle"><br></div>
+                            <h3 class="hndle"><span><?php _e( 'Yelp Widget Pro Introduction', 'ywp' ); ?></span></h3>
 
-							</div>
-							<!-- /.inside -->
-						</div>
-						<!-- /#yelp-widget-intro -->
+                            <div class="inside">
+                                <p>
+									<?php
+									$widgets_url = admin_url( 'widgets.php' );
+									$link        = sprintf( wp_kses( __( 'Thanks for choosing Yelp Widget Pro! To get started, head on over to your <a href="%s">Widgets page</a> and add Yelp Widget Pro to one of your active widget areas.', 'ywp' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( $widgets_url ) );
+									echo $link;
+									?>
+                                </p>
 
-						<div class="postbox" id="yelp-widget-options">
+                                <p><strong><?php _e( 'Need Support?', 'ywp' ); ?></strong></p>
 
-							<h3 class="hndle"><span>Yelp Widget Pro Settings</span></h3>
+                                <p><?php _e( 'If you have any problems with this plugin or ideas for improvements, please use the <a href="https://wordpress.org/support/plugin/yelp-widget-pro">WordPress.org Support Forums</a> where you can search the existing topics or create one of your own.', 'ywp' ); ?></p>
 
-							<div class="inside">
-								<div class="control-group">
-									<div class="control-label">
-										<label for="yelp_widget_disable_css">Disable Plugin CSS Output:<img src="<?php echo YELP_WIDGET_PRO_URL . '/includes/images/help.png' ?>" title="<?php _e( 'Disabling the widget\'s CSS output is useful for more complete control over customizing the widget styles. Helpful for integration into custom theme designs.', 'ywp' ); ?>" class="tooltip-info" width="16" height="16" /></label>
-									</div>
-									<div class="controls">
-										<input type="checkbox" id="yelp_widget_disable_css" name="yelp_widget_settings[yelp_widget_disable_css]" value="1" <?php
+                                <p>
+                                    <strong><?php _e( 'Like this plugin? Follow along with WordImpress:', 'ywp' ); ?></strong>
+                                </p>
+
+                                <div class="social-items-wrap">
+
+                                    <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FWordImpress%2F353658958080509&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=21&amp;appId=220596284639969"
+                                            scrolling="no" frameborder="0"
+                                            style="border:none; overflow:hidden; width:100px; height:21px;"
+                                            allowTransparency="true"></iframe>
+
+                                    <a href="https://twitter.com/wordimpress" class="twitter-follow-button"
+                                       data-show-count="false">Follow @wordimpress</a>
+                                    <script>!function (d, s, id) {
+                                            var js, fjs = d.getElementsByTagName(s)[0],
+                                                p = /^http:/.test(d.location) ? 'http' : 'https';
+                                            if (!d.getElementById(id)) {
+                                                js = d.createElement(s);
+                                                js.id = id;
+                                                js.src = p + '://platform.twitter.com/widgets.js';
+                                                fjs.parentNode.insertBefore(js, fjs);
+                                            }
+                                        }(document, 'script', 'twitter-wjs');</script>
+                                    <div class="google-plus">
+                                        <!-- Place this tag where you want the +1 button to render. -->
+                                        <div class="g-plusone" data-size="medium" data-annotation="inline"
+                                             data-width="200"
+                                             data-href="https://plus.google.com/117062083910623146392"></div>
+
+
+                                        <!-- Place this tag after the last +1 button tag. -->
+                                        <script type="text/javascript">
+                                            (function () {
+                                                var po = document.createElement('script');
+                                                po.type = 'text/javascript';
+                                                po.async = true;
+                                                po.src = 'https://apis.google.com/js/plusone.js';
+                                                var s = document.getElementsByTagName('script')[0];
+                                                s.parentNode.insertBefore(po, s);
+                                            })();
+                                        </script>
+                                    </div>
+                                    <!--/.google-plus -->
+                                </div>
+                                <!--/.social-items-wrap -->
+
+                            </div>
+                            <!-- /.inside -->
+                        </div>
+                        <!-- /#yelp-widget-intro -->
+
+                        <div class="postbox" id="yelp-widget-options">
+
+                            <h3 class="hndle"><span>Yelp Widget Pro Settings</span></h3>
+
+                            <div class="inside">
+                                <div class="control-group">
+                                    <div class="control-label">
+                                        <label for="yelp_widget_disable_css">Disable Plugin CSS Output:<img
+                                                    src="<?php echo YELP_WIDGET_PRO_URL . '/includes/images/help.png' ?>"
+                                                    title="<?php _e( 'Disabling the widget\'s CSS output is useful for more complete control over customizing the widget styles. Helpful for integration into custom theme designs.', 'ywp' ); ?>"
+                                                    class="tooltip-info" width="16" height="16"/></label>
+                                    </div>
+                                    <div class="controls">
+                                        <input type="checkbox" id="yelp_widget_disable_css"
+                                               name="yelp_widget_settings[yelp_widget_disable_css]" value="1" <?php
 										$cssOption = empty( $options['yelp_widget_disable_css'] ) ? '' : $options['yelp_widget_disable_css'];
 										checked( 1, $cssOption ); ?> />
-									</div>
-								</div>
-								<!--/.control-group -->
+                                    </div>
+                                </div>
+                                <!--/.control-group -->
 
-							</div>
-							<!-- /.inside -->
-						</div>
-						<!-- /#yelp-widget-options -->
+                            </div>
+                            <!-- /.inside -->
+                        </div>
+                        <!-- /#yelp-widget-options -->
 
-						<div class="control-group">
-							<div class="controls">
-								<input class="button-primary" type="submit" name="submit-button" value="<?php _e( 'Update', 'ywp' ); ?>" />
-							</div>
-						</div>
-					</div>
-					<!-- /#main-sortables -->
-				</div>
-				<!-- /.postbox-container -->
-				<div class="alignright" style="width:24%">
-					<div id="sidebar-sortables" class="meta-box-sortables ui-sortable">
+                        <div class="control-group">
+                            <div class="controls">
+                                <input class="button-primary" type="submit" name="submit-button"
+                                       value="<?php _e( 'Update', 'ywp' ); ?>"/>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /#main-sortables -->
+                </div>
+                <!-- /.postbox-container -->
+                <div class="alignright" style="width:24%">
+                    <div id="sidebar-sortables" class="meta-box-sortables ui-sortable">
 
-						<div id="yelp-widget-pro-premium" class="postbox">
-							<div class="handlediv" title="Click to toggle"><br></div>
-							<h3 class="hndle"><span><?php _e( 'Yelp Widget Premium', 'ywp' ); ?></span></h3>
+                        <div id="yelp-widget-pro-premium" class="postbox">
+                            <div class="handlediv" title="Click to toggle"><br></div>
+                            <h3 class="hndle"><span><?php _e( 'Yelp Widget Premium', 'ywp' ); ?></span></h3>
 
-							<div class="inside">
+                            <div class="inside">
 
-								<p><?php _e( '<a href="http://wordimpress.com/plugins/yelp-widget-pro/">Yelp Widget Premium</a> is a significant upgrade to Yelp Widget Pro that adds features such as Yelp review lists, Google Maps, and more!', 'ywp' ); ?>.</p>
+                                <p><?php _e( '<a href="http://wordimpress.com/plugins/yelp-widget-pro/">Yelp Widget Premium</a> is a significant upgrade to Yelp Widget Pro that adds features such as Yelp review lists, Google Maps, and more!', 'ywp' ); ?>
+                                    .</p>
 
-								<p><?php _e( 'Also included is Priority Support, updates, and well-documented shortcodes to display Yelp in any page or post', 'ywp' ); ?>.</p>
-							</div>
-						</div>
-						<!-- /.premium-metabox -->
+                                <p><?php _e( 'Also included is Priority Support, updates, and well-documented shortcodes to display Yelp in any page or post', 'ywp' ); ?>
+                                    .</p>
+                            </div>
+                        </div>
+                        <!-- /.premium-metabox -->
 
-					</div>
-					<!-- /.sidebar-sortables -->
+                    </div>
+                    <!-- /.sidebar-sortables -->
 
-					<a href="http://wordimpress.com/" class="wordimpress-link" target="_blank"></a>
+                    <a href="http://wordimpress.com/" class="wordimpress-link" target="_blank"></a>
 
-				</div>
-				<!-- /.alignright -->
-			</div>
-			<!-- /.metabox-holder -->
-		</form>
+                </div>
+                <!-- /.alignright -->
+            </div>
+            <!-- /.metabox-holder -->
+        </form>
 
 
-	</div><!-- /#wrap -->
+    </div><!-- /#wrap -->
 
-<?php
+	<?php
 } //end yelp_widget_options_form
 ?>
